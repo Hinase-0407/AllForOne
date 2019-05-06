ab8968899659ddc8be3ad48d3da3a5ff564d5328
fd3s0412 <fd3s0412@gmail.com>
2019-05-06 13:25:23 +0900

    エリアリスト追加.

---
+++ js/constants/m_area_list.js
@@ -0,0 +1,251 @@
+var M_AREA_LIST = [
+	{
+		areaId: "AR000",
+		landId: "LA000"
+	},
+	{
+		areaId: "AR001",
+		landId: "LA000"
+	},
+	{
+		areaId: "AR002",
+		landId: "LA000"
+	},
+	{
+		areaId: "AR003",
+		landId: "LA001"
+	},
+	{
+		areaId: "AR004",
+		landId: "LA001"
+	},
+	{
+		areaId: "AR005",
+		landId: "LA001"
+	},
+	{
+		areaId: "AR006",
+		landId: "LA001"
+	},
+	{
+		areaId: "AR007",
+		landId: "LA001"
+	},
+	{
+		areaId: "AR008",
+		landId: "LA001"
+	},
+	{
+		areaId: "AR009",
+		landId: "LA002"
+	},
+	{
+		areaId: "AR010",
+		landId: "LA002"
+	},
+	{
+		areaId: "AR011",
+		landId: "LA002"
+	},
+	{
+		areaId: "AR012",
+		landId: "LA002"
+	},
+	{
+		areaId: "AR013",
+		landId: "LA002"
+	},
+	{
+		areaId: "AR014",
+		landId: "LA002"
+	},
+	{
+		areaId: "AR015",
+		landId: "LA002"
+	},
+	{
+		areaId: "AR016",
+		landId: "LA003"
+	},
+	{
+		areaId: "AR017",
+		landId: "LA003"
+	},
+	{
+		areaId: "AR018",
+		landId: "LA003"
+	},
+	{
+		areaId: "AR019",
+		landId: "LA003"
+	},
+	{
+		areaId: "AR020",
+		landId: "LA003"
+	},
+	{
+		areaId: "AR021",
+		landId: "LA003"
+	},
+	{
+		areaId: "AR022",
+		landId: "LA003"
+	},
+	{
+		areaId: "AR023",
+		landId: "LA004"
+	},
+	{
+		areaId: "AR024",
+		landId: "LA004"
+	},
+	{
+		areaId: "AR025",
+		landId: "LA004"
+	},
+	{
+		areaId: "AR026",
+		landId: "LA004"
+	},
+	{
+		areaId: "AR027",
+		landId: "LA004"
+	},
+	{
+		areaId: "AR028",
+		landId: "LA004"
+	},
+	{
+		areaId: "AR029",
+		landId: "LA004"
+	},
+	{
+		areaId: "AR030",
+		landId: "LA004"
+	},
+	{
+		areaId: "AR031",
+		landId: "LA004"
+	},
+	{
+		areaId: "AR032",
+		landId: "LA005"
+	},
+	{
+		areaId: "AR033",
+		landId: "LA005"
+	},
+	{
+		areaId: "AR034",
+		landId: "LA005"
+	},
+	{
+		areaId: "AR035",
+		landId: "LA005"
+	},
+	{
+		areaId: "AR036",
+		landId: "LA006"
+	},
+	{
+		areaId: "AR037",
+		landId: "LA006"
+	},
+	{
+		areaId: "AR038",
+		landId: "LA006"
+	},
+	{
+		areaId: "AR039",
+		landId: "LA006"
+	},
+	{
+		areaId: "AR040",
+		landId: "LA007"
+	},
+	{
+		areaId: "AR041",
+		landId: "LA007"
+	},
+	{
+		areaId: "AR042",
+		landId: "LA007"
+	},
+	{
+		areaId: "AR043",
+		landId: "LA008"
+	},
+	{
+		areaId: "AR044",
+		landId: "LA008"
+	},
+	{
+		areaId: "AR045",
+		landId: "LA009"
+	},
+	{
+		areaId: "AR046",
+		landId: "LA009"
+	},
+	{
+		areaId: "AR047",
+		landId: "LA009"
+	},
+	{
+		areaId: "AR048",
+		landId: "LA009"
+	},
+	{
+		areaId: "AR049",
+		landId: "LA010"
+	},
+	{
+		areaId: "AR050",
+		landId: "LA010"
+	},
+	{
+		areaId: "AR051",
+		landId: "LA010"
+	},
+	{
+		areaId: "AR052",
+		landId: "LA010"
+	},
+	{
+		areaId: "AR053",
+		landId: "LA010"
+	},
+	{
+		areaId: "AR054",
+		landId: "LA010"
+	},
+	{
+		areaId: "AR055",
+		landId: "LA010"
+	},
+	{
+		areaId: "AR056",
+		landId: "LA011"
+	},
+	{
+		areaId: "AR057",
+		landId: "LA012"
+	},
+	{
+		areaId: "AR058",
+		landId: "LA013"
+	},
+	{
+		areaId: "AR059",
+		landId: "LA013"
+	},
+	{
+		areaId: "AR060",
+		landId: "LA014"
+	},
+	{
+		areaId: "AR061",
+		landId: "LA014"
+	}
+];
+module.exports = M_AREA_LIST;
